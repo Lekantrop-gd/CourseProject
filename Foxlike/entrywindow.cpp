799c1e60966d1f45fe371eff2341feed1d32c1fa
Lekantrop <98541354+Lekantrop-gd@users.noreply.github.com>
2023-11-22 22:02:12 +0200

    Add and preferred entry screen

---
+++ Foxlike/entrywindow.cpp
@@ -0,0 +1,23 @@
+#include "entrywindow.h"
+#include "ui_entrywindow.h"
+#include <QPixmap>
+#include <QIcon>
+
+EntryWindow::EntryWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::EntryWindow)
+{
+    ui->setupUi(this);
+
+    this->setWindowTitle("Foxlike Games");
+    this->setWindowIcon(QIcon("../UI/Resources/Logo.ico"));
+
+    QPixmap logoImage("../UI/Resources/Logo.png");
+    ui->logo->setPixmap(logoImage.scaled(150, 150, Qt::KeepAspectRatio));
+}
+
+EntryWindow::~EntryWindow()
+{
+    delete ui;
+}
+
---
+++ Foxlike/entrywindow.h
@@ -0,0 +1,26 @@
+#ifndef ENTRYWINDOW_H
+#define ENTRYWINDOW_H
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class EntryWindow; }
+QT_END_NAMESPACE
+
+class EntryWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    EntryWindow(QWidget *parent = nullptr);
+    ~EntryWindow();
+
+private:
+    Ui::EntryWindow *ui;
+    const int DEFAULT_WINDOW_WIDTH = 1280;
+    const int DEFAULT_WINDOW_HEIGHT = 720;
+    const QString STYLE_SHEET = "QMainWindow {"
+                                "   background-color: 17, 17, 17;"
+                                "}";
+};
+#endif // ENTRYWINDOW_H
